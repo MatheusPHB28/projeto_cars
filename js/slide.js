cd546fdbb1b0d6decca9a4e992e7d7f93c1b4a24
MatheusPHB28 <matheus.k.sousa6@aluno.senai.br>
2024-09-20 10:41:13 -0300

    projeto indo

---
+++ js/slide.js
@@ -0,0 +1,16 @@
+var slideshow = document.querySelectorAll('[data-component=slideshow"]');
+
+slideshow.forEach(initslidershow);
+
+function initslidershow (slidershow) {
+    var slides =document.querySelectorAll(´#${slideshow.id}[role="list"].slide´);
+
+    var index = 0, time = 5000;
+    slides{index}.classList.remove('active');
+
+    index++;
+
+    if (index === slides.length) index = 0
+    slides
+
+}
